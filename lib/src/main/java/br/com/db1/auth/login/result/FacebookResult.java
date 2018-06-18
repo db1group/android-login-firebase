c9362c8be58df74e1423699afc3aa8e7930f030e
vinicius.camargo <vinicamarggo@hotmail.com>
2018-06-18 12:24:18 -0300

    Melhora na personalizacao das cores

--- lib/src/main/java/br/com/db1/auth/login/result/FacebookResult.java
+++ lib/src/main/java/br/com/db1/auth/login/result/FacebookResult.java
@@ -1,5 +1,9 @@
 package br.com.db1.auth.login.result;
 
+
+/**
+ * Created by vinicius.camargo on 07/05/2017.
+ */
 public class FacebookResult {
     public String token;
     public String userId;
